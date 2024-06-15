a333945989eea1c49c509b828ad61e232507d24f
tleTerror <121960613+tleTerror@users.noreply.github.com>
2024-06-15 21:32:41 +0530

    Added the nodemon package-> so as we do not need to run and terminate the "npm run server " command evertime we make any change to code

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app=express();
 
 app.get("/",(req, res)=>{
     //root route http://localhost:5000/
-    res.send("Hello World");
+    res.send("Hello World!");
 });
 
 app.listen(5000,()=> console.log("Server Running on port 5000"));
