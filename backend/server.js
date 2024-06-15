25712fd150a0ebeb2c95a7dff92208df56762bdd
tleTerror <121960613+tleTerror@users.noreply.github.com>
2024-06-15 21:48:01 +0530

    Commit Changes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
-const express=require("express");
+// const express=require("express");
 
+import express from "express";
+import dotenv from "dotenv";
+// const dotenv=require("dotenv");
 const app=express();
+dotenv.config();
+const PORT=process.env.PORT || 5000;
 
 app.get("/",(req, res)=>{
     //root route http://localhost:5000/
     res.send("Hello World!");
 });
 
-app.listen(5000,()=> console.log("Server Running on port 5000"));
+app.listen(PORT,()=> console.log(`Server Running on port ${PORT}`));
