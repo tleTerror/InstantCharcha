9357b181b0e2f06a8851424c8f030cfb306ee85a
tleTerror <121960613+tleTerror@users.noreply.github.com>
2024-07-04 11:25:24 +0530

    committed

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,15 @@ app.get("/",(req, res)=>{
     res.send("Hello World!");
 });
 
+app.get("/api/auth/signup",(req,res) =>{
+    console.log("signup route");
+});
+
+app.get("/api/auth/login",(req,res) =>{
+    console.log("login route");
+});
+
+app.get("/api/auth/logout",(req,res) =>{
+    console.log("logout  route");
+});
 app.listen(PORT,()=> console.log(`Server Running on port ${PORT}`));
