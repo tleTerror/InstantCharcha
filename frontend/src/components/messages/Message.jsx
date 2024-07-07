b565362ceaa406ee77913ea91c60c02f44059799
tleTerror <121960613+tleTerror@users.noreply.github.com>
2024-07-07 15:10:27 +0530

    UI Done

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,21 @@
+
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+        <div className="chat-image avatar">
+            <div className="w-10 rounded-full">
+            <img
+        alt="Tailwind CSS chat bubble component"
+        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+
+            </div>
+            <div className="chat-bubble">I hate you!</div>
+            <div className="chat-footer opacity-50">Seen at 12:46</div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Message
